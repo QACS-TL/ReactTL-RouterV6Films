171129fcafc9e0f4dfb93b90751be1c70551e9f9
QACS-TL <114167055+QACS-TL@users.noreply.github.com>
2023-08-24 16:51:06 +0100

    CartIsEmpty Fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom'
+import { Nav, Navbar,  NavDropdown, Form, Button } from "react-bootstrap";
+
 import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // pages
 import Home from './pages/Home'
@@ -8,31 +11,61 @@ import Films from './pages/Films'
 import FilmDetails from './pages/FilmDetails'
 
 
-function App() {
-  const [cartIsEmpty] = useState(false);
+const App = () => {
+  //const [cartIsEmpty] = useState(false);
+  const [cartIsEmpty, setCartIsEmpty] = useState(true)
   
   return (
     <div className="App">
       <BrowserRouter>
-        <nav>
-          <h1>The Wonkymotion Film Company</h1>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/films">Films</Link>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="/home" >
+              <h1>The Wonkymotion Film Company</h1>
+          </Navbar.Brand  >
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav" >
+              <Nav className="mr-auto">
+                <Nav.Link to="/" as={Link}>Home</Nav.Link>
+              </Nav>
+              <Nav className="mr-auto">
+                <NavDropdown title="Films" id="collasible-nav-dropdown">
+                  <NavDropdown.Item to="/films" as={Link}>Films</NavDropdown.Item>
+                  <NavDropdown.Item to="/most_popular" as={Link}>
+                    Best Seller
+                  </NavDropdown.Item>
+                  <NavDropdown.Item to="/about/promotions" as={Link}>Promotions</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <Form.Check 
+                    type={'checkbox'}
+                    id='default-checkbox'
+                    defaultChecked={true}
+                    label='Cart is Empty'
+                    onClick={(e) => {console.log(e.target.checked); setCartIsEmpty(!e.target.checked);  }}
+                  />
+                  <NavDropdown.Item to="/checkout" as={Link}>
+                    Check out
+                </NavDropdown.Item>
+              </NavDropdown>
+              </Nav>
+              <Nav className="mr-auto">
+                <Nav.Link to="/about" as={Link}>About</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about/*" element={<About />} />
-          <Route path="/films/id/:id" element={<FilmDetails />} />
           <Route path="/films" element={<Films />} />
-          <Route path="//films/id/:id/*" element={<FilmDetails />} />
+          <Route path="/films/id/:id/*" element={<FilmDetails />} />
           <Route path="/most_popular" element={(
-            <div>
+            <div className="content">
               <h2>
                 UK's Most Popular Film
               </h2>
               <h3>Thunderbirds Are Go!</h3>
-                <Link to={`https://www.youtube.com/watch?v=4q6z1FMS_mM`}>
+              <img src={("/Thunderbirds.jpg")} alt={"Thunderbirds Are Go"} width="400"  />
+                <Link to={"https://www.youtube.com/watch?v=4q6z1FMS_mM"}>
                   <p>Thunderbirds</p>
                 </Link>
             </div>
@@ -40,7 +73,7 @@ function App() {
           <Route path="/redirect" element={<Navigate to="/about" />} />
           <Route 
             path="/checkout" 
-            element={cartIsEmpty ? <Navigate to="/films" />: <p>CheckOut</p>} 
+            element={cartIsEmpty ? <Navigate to="/films" />: <div className="content"><p>CheckOut</p></div>} 
           />
         </Routes>
       </BrowserRouter>
